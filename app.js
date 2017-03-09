c24eb62a15c38fe891b6c401e0ab2ff5cce692bd
elmomozio <momockit@gmail.com>
2017-03-09 14:04:20 +0100

    Express body parser

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const elasticService = require('./elasticService');
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
 app.get('/status', function(req, res) {
     elasticService.status().then(() => res.send('OK')).catch((err) => res.send(404));
 });
@@ -10,8 +13,7 @@ app.get('/', function(req, res, next) {
     elasticService.init().then((resp) => res.send(resp)).catch(next);
 });
 
-app.post('/search', function(req, res) {
-    console.log("req.body : ", req.body);
+app.post('/search', function(req, res, next) {
     elasticService.search(req.body.cp).then((resp) => res.send(resp)).catch(next);
 });
 
